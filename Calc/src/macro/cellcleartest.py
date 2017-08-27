8ec46392ef28b9340dec6bf8566319a1b70cf805
pq <pq.blogspot@gmail.com>
2017-08-27 15:58:48 +0900

    HM

---
+++ Calc/src/macro/cellcleartest.py
@@ -0,0 +1,95 @@
+#!/opt/libreoffice5.2/program/python
+# -*- coding: utf-8 -*-
+import unohelper  # オートメーションには必須(必須なのはuno)。
+from com.sun.star.sheet.CellFlags import VALUE, DATETIME, STRING, ANNOTATION, FORMULA
+def enableRemoteDebugging(func):  # デバッグサーバーに接続したい関数やメソッドにつけるデコレーター。主にリスナーのメソッドのデバッグ目的。
+	if __name__ == "__main__":  # オートメーションのときはデバッグサーバーに接続しない。
+		return func
+	def wrapper(*args, **kwargs):
+		import pydevd
+		frame = None
+		doc = XSCRIPTCONTEXT.getDocument()
+		if doc:  # ドキュメントが取得できた時
+			frame = doc.getCurrentController().getFrame()  # ドキュメントのフレームを取得。
+		else:
+			currentframe = XSCRIPTCONTEXT.getDesktop().getCurrentFrame()  # モードレスダイアログのときはドキュメントが取得できないので、モードレスダイアログのフレームからCreatorのフレームを取得する。
+			frame = currentframe.getCreator()
+		if frame:   
+			import time
+			indicator = frame.createStatusIndicator()  # フレームからステータスバーを取得する。
+			maxrange = 2  # ステータスバーに表示するプログレスバーの目盛りの最大値。2秒ロスするが他に適当な告知手段が思いつかない。
+			indicator.start("Trying to connect to the PyDev Debug Server for about 20 seconds.", maxrange)  # ステータスバーに表示する文字列とプログレスバーの目盛りを設定。
+			t = 1  # プレグレスバーの初期値。
+			while t<=maxrange:  # プログレスバーの最大値以下の間。
+				indicator.setValue(t)  # プレグレスバーの位置を設定。
+				time.sleep(1)  # 1秒待つ。
+				t += 1  # プログレスバーの目盛りを増やす。
+			indicator.end()  # reset()の前にend()しておかないと元に戻らない。
+			indicator.reset()  # ここでリセットしておかないと例外が発生した時にリセットする機会がない。
+		pydevd.settrace(stdoutToServer=True, stderrToServer=True)  # デバッグサーバーを起動していた場合はここでブレークされる。
+		try:
+			func(*args, **kwargs)  # Step Intoして中に入る。
+		except:
+			import traceback; traceback.print_exc()  # これがないとPyDevのコンソールにトレースバックが表示されない。stderrToServer=Trueが必須。
+	return wrapper
+# @enableRemoteDebugging
+def macro():
+	doc = XSCRIPTCONTEXT.getDocument()
+	if not doc.supportsService("com.sun.star.sheet.SpreadsheetDocument"):  # Calcドキュメントであることを確認する。
+		raise RuntimeError("Please execute this macro with a Calc document.")
+	sheets = doc.getSheets()  # ドキュメントからSpreadsheetsを取得。
+	sheet = sheets[0]  # Spreadsheetを取得。
+# 	sheet.clearContents(VALUE+DATETIME+STRING+ANNOTATION+FORMULA)  # シートの全内容をクリア。
+	
+	
+	cell = sheet["B15"]
+# 	sheet.clearContents(FORMULA)  # シートの全内容をクリア。
+	
+	
+	sheet[0,:2].setDataArray((('みかん', 'りんご'),))  # 引数は行のセルを要素とするタプルのタプル。
+	cellrange = sheet[1:5,:2]  # SheetCellRangeを取得。
+	cellrange.setDataArray([[(i+1)*(j+2) for j in range(len(cellrange.getColumns()))] for i in range(len(cellrange.getRows()))])  # 引数はタプル以外にリストでもイテレータでもOKだが、シートの範囲と大きさと一致していないといけない。
+g_exportedScripts = macro, #マクロセレクターに限定表示させる関数をタプルで指定。
+if __name__ == "__main__":  # オートメーションで実行するとき
+	import officehelper
+	from functools import wraps
+	import sys
+	from com.sun.star.beans import PropertyValue
+	from com.sun.star.script.provider import XScriptContext  
+	def connectOffice(func):  # funcの前後でOffice接続の処理
+		@wraps(func)
+		def wrapper():  # LibreOfficeをバックグラウンドで起動してコンポーネントテクストとサービスマネジャーを取得する。
+			try:
+				ctx = officehelper.bootstrap()  # コンポーネントコンテクストの取得。
+			except:
+				print("Could not establish a connection with a running office.", file=sys.stderr)
+				sys.exit()
+			print("Connected to a running office ...")
+			smgr = ctx.getServiceManager()  # サービスマネジャーの取得。
+			print("Using {} {}".format(*_getLOVersion(ctx, smgr)))  # LibreOfficeのバージョンを出力。
+			return func(ctx, smgr)  # 引数の関数の実行。
+		def _getLOVersion(ctx, smgr):  # LibreOfficeの名前とバージョンを返す。
+			cp = smgr.createInstanceWithContext('com.sun.star.configuration.ConfigurationProvider', ctx)
+			node = PropertyValue(Name = 'nodepath', Value = 'org.openoffice.Setup/Product' )  # share/registry/main.xcd内のノードパス。
+			ca = cp.createInstanceWithArguments('com.sun.star.configuration.ConfigurationAccess', (node,))
+			return ca.getPropertyValues(('ooName', 'ooSetupVersion'))  # LibreOfficeの名前とバージョンをタプルで返す。
+		return wrapper
+	@connectOffice  # mainの引数にctxとsmgrを渡すデコレータ。
+	def main(ctx, smgr):  # XSCRIPTCONTEXTを生成。
+		class ScriptContext(unohelper.Base, XScriptContext):
+			def __init__(self, ctx):
+				self.ctx = ctx
+			def getComponentContext(self):
+				return self.ctx
+			def getDesktop(self):
+				return ctx.getByName('/singletons/com.sun.star.frame.theDesktop')  # com.sun.star.frame.Desktopはdeprecatedになっている。
+			def getDocument(self):
+				return self.getDesktop().getCurrentComponent()
+		return ScriptContext(ctx)  
+	XSCRIPTCONTEXT = main()  # XSCRIPTCONTEXTを取得。
+	doc = XSCRIPTCONTEXT.getDocument()  # ドキュメントを取得。
+	if not hasattr(doc, "getCurrentController"):  # ドキュメント以外のとき。スタート画面も除外。
+		XSCRIPTCONTEXT.getDesktop().loadComponentFromURL("private:factory/scalc", "_blank", 0, ())  # Calcのドキュメントを開く。
+		while doc is None:  # ドキュメントのロード待ち。
+			doc = XSCRIPTCONTEXT.getDocument()
+	macro()
